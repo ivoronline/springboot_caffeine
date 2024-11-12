59b14e16c8525e9bddbb25bba93c3e1b38793180
goretaivor <ivor.goreta@primus.com.al>
2024-11-12 13:10:43 +0100

    Commit

--- src/main/java/com/ivoronline/service/PersonService.java
+++ src/main/java/com/ivoronline/service/PersonService.java
@@ -1,26 +1,44 @@
 package com.ivoronline.service;
 
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+  //PROPERTIES
+  @Autowired CacheManager cacheManager;
+
   //=========================================================================================================
   // GET PERSON
   //=========================================================================================================
-  @Cacheable("PersonCash")
   public String getPerson(int id) {
-  
-    //LOG
-    System.out.println("ENTERED getPerson: " + id);
     
-    //RETURN PERSON
-    switch (id) {
-      case  1: return "John";
-      case  2: return "Susan";
-      default: return "Unknown";
+    //LOG
+    System.out.println("\nENTERED getPerson: " + id);
+    
+    //GET FROM CACHE
+    Cache cache = cacheManager.getCache("PersonCash");
+    if(cache.get(id) != null) {
+        System.out.println("Returned from Cache");
+        return (String) cache.get(id).get();
     }
+    
+    //CALCULATE RESULT
+    String result;
+    switch (id) {
+      case  1: result = "John";  break;
+      case  2: result = "Susan"; break;
+      default: result = "Unknown";
+    }
+    
+    //PUT INTO CACHE
+    cache.put(id, result);
+    
+    //RETURN RESULT
+    return result;
     
   }
 
