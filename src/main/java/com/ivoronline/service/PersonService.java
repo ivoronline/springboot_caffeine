8c647c3c87f9d344ef42e6b875bfe835f3142318
ivoronline <ivoronline@gmail.com>
2024-11-12 15:13:18 +0100

    Commit

--- src/main/java/com/ivoronline/service/PersonService.java
+++ src/main/java/com/ivoronline/service/PersonService.java
@@ -1,10 +1,21 @@
 package com.ivoronline.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.stereotype.Service;
+import java.util.Iterator;
+import java.util.Map;
 
 @Service
 public class PersonService {
+
+  //PROPERTIES
+  @Autowired
+  CacheManager cacheManager;
 
   //=========================================================================================================
   // GET PERSON
@@ -15,11 +26,34 @@ public class PersonService {
     //LOG
     System.out.println("ENTERED getPerson: " + id);
     
+    //DISPLAY CACHE
+    printCache("PersonCash");
+    
     //RETURN PERSON
     switch (id) {
       case  1: return "John";
       case  2: return "Susan";
       default: return "Unknown";
+    }
+    
+  }
+
+  //=========================================================================================================
+  // PRINT CACHE
+  //=========================================================================================================
+  public void printCache(String cacheName) {
+  
+    //GET MAP FROM CACHE
+    CaffeineCacheManager caffeineCacheManager = (CaffeineCacheManager) cacheManager; //Get implementation
+    CaffeineCache caffeineCache = (CaffeineCache) caffeineCacheManager.getCache(cacheName);
+    Cache caffeine = caffeineCache.getNativeCache();
+    Map<Object, Object> map = caffeine.asMap();
+    
+    //ITERATE THROUGH KEYS
+    Iterator iterator = map.keySet().iterator();
+    while (iterator.hasNext() == true) {
+      int key = (int) iterator.next();
+      System.out.println(key + " - " + caffeineCache.get(key).get());
     }
     
   }
