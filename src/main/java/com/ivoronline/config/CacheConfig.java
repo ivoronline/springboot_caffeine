4c5ab120bb09fa556fcff461dcff6c1d70ad4a1b
ivoronline <ivoronline@gmail.com>
2024-11-12 15:05:57 +0100

    Commit

--- src/main/java/com/ivoronline/config/CacheConfig.java
+++ src/main/java/com/ivoronline/config/CacheConfig.java
@@ -1,35 +1,21 @@
 package com.ivoronline.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
   //=========================================================================================================
-  // CAFFEINE CONFIG
-  //=========================================================================================================
-  @Bean
-  public Caffeine caffeineConfig() {
-    return Caffeine.newBuilder()
-      .expireAfterWrite(60, TimeUnit.MINUTES)
-      .initialCapacity(10);
-  }
-  
-  //=========================================================================================================
   // CACHE MANAGER
   //=========================================================================================================
   @Bean
-  public CacheManager cacheManager(Caffeine caffeine) {
-    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-                         caffeineCacheManager.setCaffeine(caffeine);
-    return               caffeineCacheManager;
+  public CacheManager cacheManager() {
+    return new CaffeineCacheManager();
   }
 
 }
--- src/main/java/com/ivoronline/service/PersonService.java
+++ src/main/java/com/ivoronline/service/PersonService.java
@@ -1,19 +1,42 @@
 package com.ivoronline.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import java.util.Iterator;
+import java.util.Map;
 
 @Service
 public class PersonService {
 
+  //PROPERTIES
+  @Autowired CacheManager cacheManager;
+
   //=========================================================================================================
   // GET PERSON
   //=========================================================================================================
-  @Cacheable("PersonCash")
+  @Cacheable(value = "PersonCash", unless="#result == 'Unknown'")
   public String getPerson(int id) {
   
     //LOG
-    System.out.println("ENTERED getPerson: " + id);
+    System.out.println("\nENTERED getPerson: " + id);
+    
+    //GET MAP FROM CACHE
+    CaffeineCacheManager caffeineCacheManager = (CaffeineCacheManager) cacheManager; //Get implementation
+    CaffeineCache        caffeineCache        = (CaffeineCache) caffeineCacheManager.getCache("PersonCash");
+    Cache                caffeine             = caffeineCache.getNativeCache();
+    Map<Object, Object>  map                  = caffeine.asMap();
+
+    //ITERATE THROUGH KEYS
+    Iterator iterator = map.keySet().iterator();
+    while( iterator.hasNext() == true ) {
+      int key = (int) iterator.next();
+      System.out.println(key + " - " + caffeineCache.get(key).get());
+    }
     
     //RETURN PERSON
     switch (id) {
